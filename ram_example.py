ccd3ca37a8189a28c99eabc5e9c3138de0e84139
Matheus Pedroni <pnmatheus@protonmail.com>
2023-07-02 22:03:33 -0300

    RAM tag extraction example

---
+++ ram_example.py
@@ -0,0 +1,37 @@
+import argparse
+import numpy as np
+
+import torch
+
+from PIL import Image
+from ram.models import ram
+from ram import inference_ram as inference
+from ram import get_transform
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Tag2Text inferece for tagging and captioning")
+    parser.add_argument("image")
+    parser.add_argument(
+        "--image-size", default=384, type=int, metavar="N", help="input image size (default: 448)"
+    )
+
+    args = parser.parse_args()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    transform = get_transform(image_size=args.image_size)
+
+    model = ram(pretrained="./models/ram_swin_large_14m.pth", image_size=args.image_size, vit="swin_l")
+    model.eval()
+
+    model = model.to(device)
+
+    image = transform(Image.open(args.image)).unsqueeze(0).to(device)
+
+    res = inference(image, model)
+    print(res[0])
+
+
+if __name__ == "__main__":
+    main()
