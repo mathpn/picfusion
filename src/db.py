79a825e03327c25feeb24d883becab418250b421
Matheus Pedroni <pnmatheus@protonmail.com>
2023-07-12 21:49:56 -0300

    CLIP index implementation

--- src/db.py
+++ src/db.py
@@ -4,9 +4,9 @@ import os
 from hashlib import sha1
 from typing import Optional
 
-from PIL import Image
+import numpy as np
 
-from src.index import TagIndex
+from src.index import TagIndex, ClipIndex
 
 
 class StorageDB:
@@ -23,6 +23,14 @@ class StorageDB:
             )
             """
         )
+        self.conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS features (
+                id TEXT PRIMARY KEY, features BLOB, FOREIGN KEY (id) REFERENCES images (id)
+            )
+            """
+        )
+        self.conn.commit()
 
     def insert_image(self, img_bytes: bytes, extension: str) -> str:
         img_id = sha1(img_bytes).hexdigest()
@@ -32,11 +40,18 @@ class StorageDB:
         )
         return img_id
 
-    def insert_tags(self, img_id: str, tags: list[str]):
-        tags_json = json.dumps(tags)
+    def insert_tags(self, img_id: str, tags: set[str]):
+        tags_json = json.dumps(list(tags))
         self.conn.execute(
             "INSERT INTO tags (id, tags) VALUES (?, ?) ON CONFLICT (id) DO UPDATE SET tags = ?",
             (img_id, tags_json, tags_json),
+        )
+
+    def insert_features(self, img_id: str, features: np.ndarray) -> None:
+        feature_bytes = features.tobytes()
+        self.conn.execute(
+            "INSERT INTO features (id, features) VALUES (?, ?) ON CONFLICT DO UPDATE SET features = ?",
+            (img_id, feature_bytes, feature_bytes),
         )
 
     def create_tag_index(self) -> TagIndex:
@@ -47,9 +62,23 @@ class StorageDB:
             img_tags.append(set(json.loads(tags)))
         return TagIndex(img_ids, img_tags)
 
+    def create_clip_index(self) -> ClipIndex:
+        cur = self.conn.execute("SELECT id, features FROM features")
+        img_ids, img_features = [], []
+        for img_id, blob in cur:
+            # XXX dtype
+            feat = np.frombuffer(blob, dtype=np.float32)
+            img_ids.append(img_id)
+            img_features.append(feat)
+        return ClipIndex(img_ids, np.stack(img_features))
+
     def retrieve_img(self, img_id: str) -> Optional[tuple[str, bytes]]:
         cur = self.conn.execute("SELECT extension, content FROM images WHERE id = ?", (img_id,))
         return cur.fetchone()
 
     def close(self):
+        self.commit()
+        self.conn.close()
+
+    def commit(self):
         self.conn.commit()
--- src/index.py
+++ src/index.py
@@ -1,11 +1,20 @@
 from typing import Iterable
 
+import numpy as np
+
+
 def argsort(seq, ascending: bool = True):
     # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
-    sorted_seq = sorted(range(len(seq)), key= seq.__getitem__)
+    sorted_seq = sorted(range(len(seq)), key=seq.__getitem__)
     if ascending:
         return sorted_seq
     return list(reversed(sorted_seq))
+
+
+def cos_sim(a, b):
+    a /= np.linalg.norm(a, axis=1, keepdims=True)
+    b /= np.linalg.norm(b, axis=1, keepdims=True)
+    return a @ b.T
 
 
 class TagIndex:
@@ -18,4 +27,16 @@ class TagIndex:
             query_tags = set(query_tags)
         overlap = [len(query_tags & tags) for tags in self.tags]
         sort_idx = argsort(overlap, ascending=False)
+        return [self.img_ids[i] for i in sort_idx[:top_k]]
+
+
+class ClipIndex:
+    def __init__(self, img_ids: list[str], features: np.ndarray):
+        self.img_ids = img_ids
+        self.features = features
+
+    def find_knn(self, query_feats: np.ndarray, top_k: int) -> list[str]:
+        # XXX features must be of unit norm!
+        sim = query_feats.reshape(1, -1) @ self.features.T
+        sort_idx = np.argsort(-sim[0])
         return [self.img_ids[i] for i in sort_idx[:top_k]]
