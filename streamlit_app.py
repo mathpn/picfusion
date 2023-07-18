fafd709ea0306f326c2be215c89c8123d82d45dd
Matheus Pedroni <pnmatheus@protonmail.com>
2023-07-17 21:15:18 -0300

    add simple image gallery to streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
-import torch
+import io
+
 import streamlit as st
-from PIL import Image
+import torch
+from PIL import Image, ImageOps
 
 from src.db import StorageDB
 from src.inference import create_clip_extractor, create_ram_extractor
-
 
 MODEL_PATH = "./models/ram_swin_large_14m.pth"  # FIXME hardcoded
 TAG_FILE_PATH = "./ram/data/ram_tag_list.txt"
@@ -20,19 +21,14 @@ def get_valid_tags(tag_file_path: str) -> set[str]:
 def get_searcher(model_path: str, valid_tags: set[str], device):
     clip_img_extractor, clip_txt_extractor = create_clip_extractor(device)
     ram_extractor = create_ram_extractor(model_path, device=device)
-    db = StorageDB("./storage.db")  # FIXME hardcoded
+    db = StorageDB("./storage.db", read_mode=True)  # FIXME hardcoded
     index = db.create_index()
-    print("hey")
 
     def do_search(img_file=None, input_tags=None, text=None, top_k: int = 10):
-        print("searching")
-
         tags = set()
         feat, img_feat, txt_feat = None, None, None
         if img_file is not None:
-            print(img_file)
             img = Image.open(img_file)
-            print(img)
             tags = ram_extractor([img])[0]
             img_feat = clip_img_extractor([img])[0]
 
@@ -40,27 +36,23 @@ def get_searcher(model_path: str, valid_tags: set[str], device):
             txt_feat = clip_txt_extractor([text])
 
         if img_feat is not None and txt_feat is not None:
-            print("a")
             feat = (img_feat + txt_feat) / 2
         elif img_feat is not None and txt_feat is None:
-            print("b")
             feat = img_feat
         else:
-            print("c")
             feat = txt_feat
 
         input_tags = set(input_tags or [])
         tags = tags | (input_tags & valid_tags)
         if tags and feat is not None:
-            print(1)
             top_ids = index.find_knn_combined(tags, feat, top_k)
         elif not tags and feat is not None:
-            print(2)
             top_ids = index.find_knn_clip(feat, top_k)
         else:
-            print(3)
             top_ids = index.find_knn_tags(tags, top_k)
-        return top_ids
+
+        img_info = [(*db.retrieve_img(img_id), score) for img_id, score in top_ids]
+        return img_info
 
     return do_search
 
@@ -71,14 +63,26 @@ def main():
     valid_tags = get_valid_tags(TAG_FILE_PATH)
     searcher = get_searcher(MODEL_PATH, valid_tags, device)
 
-    file_upload = st.file_uploader("Upload image:")
+    file_upload = st.file_uploader("Upload image:", accept_multiple_files=False)
     tags_selector = st.multiselect("Desired tags", options=sorted(valid_tags))
-    txt_descriptor = st.text_input("Text description:")
-    search_button = st.button("Search")
+    txt_descriptor = st.text_input("Text description:", max_chars=256)
+    top_k = st.slider("Number of images to retrieve:", min_value=1, max_value=100, value=12)
+    search_button = st.button("Search Images")
+    img_info = []
     if search_button:
-        result = searcher(file_upload, tags_selector, txt_descriptor)
-        print(result)
-        # TODO image grid
+        img_info = searcher(file_upload, tags_selector, txt_descriptor, top_k)
+
+    groups = []
+    n = 4
+    for i in range(0, len(img_info), n):
+        groups.append(img_info[i : i + n])
+
+    for group in groups:
+        cols = st.columns(n)
+        for i, (_, img_bytes, score) in enumerate(group):
+            img = Image.open(io.BytesIO(img_bytes))
+            img = ImageOps.exif_transpose(img)
+            cols[i].image(img, caption=f"{score:.3f}", output_format="JPEG")
 
 
 if __name__ == "__main__":
